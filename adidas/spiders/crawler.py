72f4c45f02f4cd5bce01f8bdde8b33f8fc0553c8
Duong Nguy <duongnt.cnthongtin@gmail.com>
2023-12-13 11:40:28 +0700

    init

---
+++ adidas/spiders/crawler.py
@@ -0,0 +1,70 @@
+import scrapy
+from scrapy.http import Request
+from scrapy_playwright.handler import PageMethod
+
+
+class CrawlerSpider(scrapy.Spider):
+    name = "crawler"
+    # allowed_domains = "www.adidas.com"
+    # start_urls = ["https://www.adidas.com/us/"]
+
+    def start_requests(self):
+        # urls = ['https://www.adidas.com/us/men-clothing',
+        #         'https://www.adidas.com/us/men-shoes',
+        #         'https://www.adidas.com/us/men-accessories',
+        #         'https://www.adidas.com/us/women-clothing',
+        #         'https://www.adidas.com/us/women-shoes',
+        #         'https://www.adidas.com/us/women-accessories',
+        #         'https://www.adidas.com/us/boys-clothing',
+        #         'https://www.adidas.com/us/boys-shoes',
+        #         'https://www.adidas.com/us/girls-clothing',
+        #         'https://www.adidas.com/us/girls-shoes',
+        #         # 'https://www.adidas.com/us/kids-infant_toddler',
+        #         'https://www.adidas.com/us/kids-accessories']
+        
+        # for url in urls:
+        #     yield scrapy.Request(url, meta=dict(playwright=True, playwright_include_page=True,
+        #                                     errback=self.errback))
+        url = 'https://www.adidas.com/us/boys-shoes'
+        yield scrapy.Request(url, meta=dict(playwright=True, playwright_include_page=True,
+                                            errback=self.errback))
+
+
+    async def parse(self, response):
+        page = response.meta["playwright_page"]
+        await page.close()
+
+        container = response.xpath(
+            '//*[@id="main-content"]/div/div[3]/div/div/div[2]/div[1]/div')
+        items = container.css('div.grid-item')
+        for item in items:
+            try:
+                yield {
+                    'name': item.css('div > div > div > a > div > p.glass-product-card__title::text').get(),
+                    'url': "https://www.adidas.com/" + item.css('div > div > div > div > a.glass-product-card__assets-link::attr(href)').get(),
+                    'images': item.css('div > div > div > div > a.glass-product-card__assets-link > img::attr(src)').get()
+                }
+            except Exception as e:
+                self.logger.error(f"Error at {item}, {e}")
+        try:
+            next_page = response.css(
+                '#main-content > div > div:nth-child(3) > div > div > div.col-s-12.col-l-24.no-x-padding-on-mobile___3dhA9 > div.pagination--container___3gNjN > div > div > div > div.pagination__control___3C268.pagination__control--next___329Qo.pagination_margin--next___3H3Zd > a::attr(href)').get()
+            if next_page is not None:
+                try:
+                    next_page_url = "https://www.adidas.com" + next_page
+                    self.logger.info(f'Requested at {next_page_url}')
+                    yield scrapy.Request(next_page_url, meta=dict(
+                        playwright=True,
+                        playwright_include_page=True,
+                        errback=self.errback,
+                    ))
+                except Exception as e:
+                    self.logger.error(f"Error at next page is none'/n', {e}")
+        except Exception as e:
+            self.logger.error(f"No next page found, {e}")
+
+    async def errback(self, failure):
+        page = failure.request.meta["playwright_page"]
+        await page.close()
+
+
---
+++ adidas/spiders/details.py
@@ -0,0 +1,100 @@
+import scrapy
+from scrapy.http import Request
+from scrapy_playwright.handler import PageMethod
+from datetime import datetime
+import pandas as pd
+
+
+class CrawlerSpider(scrapy.Spider):
+    name = "detail"
+    allowed_domains = "www.adidas.com"
+    # start_urls = ["https://www.adidas.com/us/"]
+
+    def start_requests(self):
+        # global url
+        # url = "https://www.adidas.com/us/adidas-by-stella-mccartney-tote/HY4086.html"
+
+        # yield scrapy.Request(url, meta=dict(playwright=True, playwright_include_page=True,
+        #                                     playwright_page_methods=[
+        #                                         PageMethod('click',  '//*[@id="fixed-content"]/section[2]'),
+        #                                     ],
+        #                                     errback=self.errback))
+        df = pd.read_csv('boyshoe.csv')
+        for url in df['url']:
+            try:
+                yield scrapy.Request(url, meta=dict(playwright=True, playwright_include_page=True,
+                                                    playwright_page_methods=[
+                                                        PageMethod(
+                                                            "wait_for_selector", "#fixed-content > section:nth-child(4)"),
+                                                        PageMethod(
+                                                            'click',  '//*[@id="fixed-content"]/section[2]'),
+                                                    ],
+                                                    errback=self.errback))
+            except Exception as e:
+                self.logger.error(f'Error at {url}, problem at {e}')
+
+    async def parse(self, response):
+        page = response.meta["playwright_page"]
+        await page.close()
+        # check if have more than one color
+        color_available = response.xpath(
+            '//*[@id="available-colors-label"]/text()').get()
+        color = None
+
+        if color_available:
+            color = response.xpath(
+                '//*[@id="main-content"]/div[2]/div[2]/div[2]/div[3]/text()').get()
+        else:
+            color = response.xpath(
+                '//*[@id="main-content"]/div[2]/div[2]/div[2]/text()').get()
+        # check if price has discount
+        price = response.xpath(
+            '//*[@id="main-content"]/div[2]/div[2]/div[1]/div[2]/div/div/div/div[2]/text()').get()
+        if price:
+            price = response.xpath(
+                '//*[@id="main-content"]/div[2]/div[2]/div[1]/div[2]/div/div/div/div[2]/text()').get().replace("$", "")
+        else:
+            price = response.xpath(
+                '//*[@id="main-content"]/div[2]/div[2]/div[1]/div[2]/div/div/div/div/text()').get().replace("$", "")
+
+        try:
+            name = response.xpath(
+                '//*[@id="main-content"]/div[2]/div[2]/div[1]/h1/span/text()').get()
+        except:
+            name = ''
+
+        try:
+            description = response.xpath(
+                '//*[@id="navigation-target-description"]/div/div/div/div/div[1]/h3/text()').get() + ' '
+            + response.xpath('//*[@id="navigation-target-description"]/div/div/div/div/div[1]/p/text()').get()
+        except:
+            description = ''
+        try:
+            review_count = response.xpath('//*[@id="navigation-target-reviews"]/div/button/div[1]/div/h2/text()').get(
+            ).replace("Reviews", "").replace('(', '').replace(')', ''),
+        except:
+            review_count = ''
+        try:
+            avg_rating = response.xpath(
+                '//*[@id="navigation-target-reviews"]/div/button/div[1]/div/div/span/text()').get(),
+        except:
+            avg_rating = ''
+        yield {
+            'name': name,
+            'price': price,
+            'description': description,
+            'review_count': review_count,
+            'avg_rating': avg_rating,
+            'scraped_at': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            'color': color,
+            'brand': 'Adidas',
+            'currency': 'USD',
+            'url': response.request.url,
+            'availability': 'INSTOCK'
+        }
+
+    async def errback(self, failure):
+        page = failure.request.meta["playwright_page"]
+        await page.close()
+
+# availability	avg_rating	brand	color	currency	description	images	name	price	review_count	scraped_at	url
